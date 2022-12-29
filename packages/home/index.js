b5a879824d90dcd929e669e5d8daf658a654f091
wppaing <wintphyopaing.p@gmail.com>
2022-12-29 07:54:44 +0630

    learning recursion

--- packages/home/index.js
+++ packages/home/index.js
@@ -1,3 +1,21 @@
-const commonFn = require("@test/common");
+//using for loop
+function sumTo(n) {
+  let result = 0;
+  for (let i = 0; i <= n; i++) {
+    result += i;
+  }
+  return result;
+}
 
-commonFn();
+//using recursion
+function sumToRecurse(n) {
+  return n === 1 ? n : n + sumToRecurse(n - 1);
+}
+
+//using formula
+function sumToFormula(n) {
+  return (n * (n + 1)) / 2;
+  return ((n + 1) / 2) * n;
+}
+
+console.log(sumToFormula(5000000000));
